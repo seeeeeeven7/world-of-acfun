3aebc2f1b175192730e63d8155e79d47e3827cf0
Steven Hu <SourceDark@gmail.com>
2018-02-13 23:16:45 +0800

    work save

---
+++ src/class/card.js
@@ -0,0 +1,24 @@
+﻿var Card = {
+    scale: 0.7,
+    createNew: function(type, x, y) {
+        var card = {
+            type: type,
+            position: {
+                x: x,
+                y: y
+            }
+        }
+        card.image = new Image();
+        card.image.src = "../images/acfun_emoji/" + type + ".png"
+        card.drawIn = function(context) {
+            var scale = 0.7;
+            context.drawImage(this.image, this.position.x, this.position.y, dw = this.image.width * scale, dh = this.image.height * scale)
+            context.font = "15px tahoma"
+            context.fillStyle = "deepgray";
+            context.fillText("HP", this.position.x + 120, this.position.y + 26)
+            context.fillText("MP", this.position.x + 120, this.position.y + 53)
+            context.fillText("EXP", this.position.x + 120, this.position.y + 80)
+        }
+        return card;
+    }
+};
---
+++ src/index.js
@@ -0,0 +1,53 @@
+﻿// Create Canvas
+var canvas = document.createElement("canvas");
+var context = canvas.getContext("2d");
+canvas.width = 800;
+canvas.height = 600;
+document.body.appendChild(canvas);
+
+var bgReady = false;
+var bgImage = new Image();
+bgImage.onload = function () {
+    bgReady = true;
+};
+bgImage.src = "../images/acfun_emoji/01.png";
+
+// cardIds = ['01', '02', '03', '05', '06'];
+cardIds = ['01', '02', '03'];
+cards = []
+for (var i in cardIds) {
+    card = Card.createNew(cardIds[i], 0, i * 100)
+    cards.push(card)
+}
+
+var reset = function () {
+    
+};
+
+var update = function (modifier) {
+    
+};
+
+var render = function () {
+    context.clearRect(0, 0, canvas.width, canvas.height);  
+    for (var i in cardIds) {
+        cards[i].drawIn(context);
+    }
+}
+
+var main = function () {
+    var now = Date.now();
+    var delta = now - then;
+    update(delta / 1000);
+    render();
+    then = now;
+    // 立即调用主函数
+    requestAnimationFrame(main);
+};
+
+var w = window;
+requestAnimationFrame = w.requestAnimationFrame || w.webkitRequestAnimationFrame || w.msRequestAnimationFrame || w.mozRequestAnimationFrame;
+
+var then = Date.now();
+reset();
+main();
